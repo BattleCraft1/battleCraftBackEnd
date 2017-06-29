897202ca4f4d18fa234d7a7f5bee3e32c7734f82
Jarek <jaroslaw.bielec@pollub.edu.pl>
2017-06-29 19:06:51 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pollub/battleCraft/BattleCraftApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pollub.battleCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleCraftApplication.class, args);
+	}
+}
