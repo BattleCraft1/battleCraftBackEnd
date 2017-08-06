e7017e00f16948e135d79088bc56c32c1a4ad596
Jarek <jaroslaw.bielec@pollub.edu.pl>
2017-08-06 19:17:32 +0200

    Dodalem exception handlera

--- src/main/java/pl/edu/pollub/battleCraft/service/aspects/GetPageOfEntityAspect.java
+++ src/main/java/pl/edu/pollub/battleCraft/service/aspects/GetPageOfEntityAspect.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
+import pl.edu.pollub.battleCraft.data.searchSpecyficators.SearchSpecification;
+import pl.edu.pollub.battleCraft.service.exceptions.AnyEntityNotFoundException;
 import pl.edu.pollub.battleCraft.service.exceptions.PageNotFoundException;
 
 import java.util.logging.Logger;
@@ -26,11 +28,18 @@ public class GetPageOfEntityAspect {
             throw new IllegalAccessException("Your page must have less than 10 elements");
     }
 
-    @AfterReturning(pointcut="execution(* pl.edu.pollub.battleCraft.data.repositories.*Repository.*(..)) && args(*,pageRequest)", returning="fetchedPage", argNames = "pageRequest,fetchedPage")
-    public void checkPageContent(PageRequest pageRequest, Page fetchedPage) throws PageNotFoundException {
+    @AfterReturning(pointcut="execution(* pl.edu.pollub.battleCraft.data.repositories.extensions.*Repository.*(..)) " +
+            "&& args(objectSearchSpecification,pageRequest)", returning="fetchedPage",
+            argNames = "objectSearchSpecification,pageRequest,fetchedPage")
+    public void checkPageContent(SearchSpecification<?> objectSearchSpecification,
+                                 PageRequest pageRequest, Page fetchedPage) throws PageNotFoundException {
         logger.info("checking page content");
-        if(!fetchedPage.hasContent())
-            throw new PageNotFoundException(pageRequest.getPageNumber());
+        if(!fetchedPage.hasContent()) {
+            if(objectSearchSpecification.getCriteries().size()>0)
+                throw new AnyEntityNotFoundException();
+            else
+                throw new PageNotFoundException(pageRequest.getPageNumber()+1);
+        }
     }
 
     public int getAllowedPageSize() {
---
+++ src/main/java/pl/edu/pollub/battleCraft/service/exceptions/AnyEntityNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.edu.pollub.battleCraft.service.exceptions;
+
+public class AnyEntityNotFoundException extends RuntimeException{
+    public AnyEntityNotFoundException(){
+        super("Any entity does not match to this search criteria");
+    }
+}
